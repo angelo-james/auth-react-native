bd3a75ca135354b96d437f6b478d59b19e1fef5d
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-01-31 00:37:30 -0800

    installed and imported firebase used componentwillmount to initialize app using firebase

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './src/components/common';
+import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyA6lBre_CWlzTTo3nCXRbUsxV8mfgM4UUY",
+      authDomain: "auth-606fb.firebaseapp.com",
+      databaseURL: "https://auth-606fb.firebaseio.com",
+      projectId: "auth-606fb",
+      storageBucket: "auth-606fb.appspot.com",
+      messagingSenderId: "1036554078324"
+    })
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>Open up App.js to start working on your app!</Text>
+        <LoginForm />
       </View>
     );
   }
