835498b4f24ceee1ab46dd951829597d15fa95d5
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-01-31 16:53:53 -0800

    added styles and props to input compnent

--- src/components/common/TextField.js
+++ src/components/common/TextField.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({ label, value, onChangeText, placeholder }) => {
+  const { inputStyle, labelStyle, containerStyle } = styles;
+
   return (
-    <View>
-      <Text>
+    <View style={containerStyle}>
+      <Text style={labelStyle}>
         {label}
       </Text>
       <TextInput
+        placeholder={placeholder}
+        autoCorrect={false}
+        style={inputStyle}
         value={value}
         onChangeText={onChangeText}
-        style={{ height: 20, width: 100 }}
       />
     </View>
   )
@@ -18,13 +22,23 @@ const Input = ({ label, value, onChangeText }) => {
 
 const styles = {
   inputStyle: {
-
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    flex: 2
   },
   labelStyle: {
-
+    fontSize: 18,
+    paddingLeft: 20,
+    flex: 1
   },
   containerStyle: {
-    
+    height: 40,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
   }
 }
 
